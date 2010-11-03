9915fda2ea4fc7bed4926522bc333ae2b3a86274
Javier Rosa <javirosa@eecs.berkeley.edu>
2010-11-02 21:51:51 -0700

    Added DbgTel library to handle controlling the DbgLeds.

---
+++ libraries/DbgTel/DbgTel.cpp
@@ -0,0 +1,32 @@
+/** @file DbgTel.cpp */
+#include "DbgTel.h"
+
+void initDbgTel()
+{
+	pinMode(GRNPIN,OUTPUT);
+	pinMode(YELPIN,OUTPUT);
+	pinMode(REDPIN,OUTPUT);
+}
+void setDbgLeds(int8_t pattern) 
+{
+	/*Just light up the LEDS for now.*/
+	if (pattern/RPAT) {
+		digitalWrite(REDPIN, HIGH);
+	} else {
+		digitalWrite(REDPIN, LOW);
+	
+	}
+
+	if (pattern/GPAT) {
+		digitalWrite(GRNPIN, HIGH);
+	} else {
+		digitalWrite(GRNPIN, LOW);
+	}
+
+	if (pattern/YPAT) {
+		digitalWrite(YELPIN, HIGH);
+	} else {
+		digitalWrite(YELPIN, LOW);
+	}
+}
+
