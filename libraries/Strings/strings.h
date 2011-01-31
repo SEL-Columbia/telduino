5700226da11600ddbe5875b26044f0051d57c952
Javier Rosa <javirosa@eecs.berkeley.edu>
2011-01-30 18:05:34 -0800

    Forgot to add strings.h

---
+++ libraries/Strings/strings.h
@@ -0,0 +1,17 @@
+#ifndef STRINGS_H
+#define STRINGS_H
+#ifdef __cplusplus__
+extern "C" {
+#endif
+
+static const char* NOACSTR = "Failed to sense cycles. Is the proper AC 50Hz source connected?";
+static const char* MVQUERYSTR = "Please enter mV.$";
+static const char* MAQUERYSTR = "Please enter mA.$";
+static const char* REPORTEDSTR = "Reported by user:";
+static const char* PRESSENTERSTR = "Press ENTER (\'\\r\') when done.$";
+static const char* CANCELEDSTR = "CANCELED";
+
+#ifdef __cplusplus__
+}
+#endif
+#endif
