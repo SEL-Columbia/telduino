78e0da549f262aaacde18a81140221843b7b4e42
Achintya Madduri <achintya.madduri@gmail.com>
2011-10-27 15:42:25 -0700

    Commit before syncing with V3

--- calcIRMSOffset.py
+++ calcIRMSOffset.py
@@ -14,15 +14,17 @@ import os
 
 
 def main():
-	v1 = 0.12
-	v2 = 2.57
-	vrms0_1 = 19200.0
-	vrms0_2 = 413000.0
-	r = v1/v2
-	offset = (r**2*vrms0_2**2-vrms0_1**2)/(1-r**2)/32768.
-	print "for v1 = ", v1, " and v2 = ", v2
-	print "vrms0_1 is: ", vrms0_1, " and vrms0_2 is: ", vrms0_2
-	print "IRMS offset is: ", offset
+	i1 = 0.5914
+	i2 = 4.4/119.9
+	irms0_1 = 593667.0
+	irms0_2 = 39325.0
+	r = i1/i2
+	offset = 1/32768.0*(i1**2*irms0_2**2-i2**2*irms0_1**2)/(i2**2-i1**2)
+	slope = (i1-i2)/(irms0_1-irms0_2)*1000
+	print "for i1 = ", i1, " and i2 = ", i2
+	print "irms0_1 is: ", irms0_1, " and irms0_2 is: ", irms0_2
+	print "IRMS offset is: ", offset, "counts"
+	print "IRMS slope is: ", slope, "mAmps/count"
 
 
 if __name__ == '__main__':
