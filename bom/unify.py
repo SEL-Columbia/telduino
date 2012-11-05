71e46da297f7a9e82c3f59711ed9361ac8254dd0
Javier Rosa <javirosa@eecs.berkeley.edu>
2012-11-05 10:34:02 +1000

    Almost fixed DB bom

--- bom/unify.py
+++ bom/unify.py
@@ -1,17 +1,21 @@
 import csv
 import struct
 
+COMPLETELINESIZE = 15
+NOTES = 13
+EAKEY = 14
+EAGLEDELIMETER = "\t"
+
 #FNAME,EAFMT = "TellitShieldV6","4s51s30s90s"
 FNAME,EAFMT = "Meter-Daughterboard-input-V6JR","4s50s27s95s"
 eacsv,dkcsv = map(lambda f: open(f,'rb'), [FNAME+".ea.csv",FNAME+".dk.csv"])
-
+outfile = open(FNAME + ".csv",'w')
 #CLEAR EA
 eacsv.readline()
 eacsv.readline()
 eacsv.readline()
 
 dkcsv.readline()
-
 
 ea = [map(str.strip,struct.unpack(EAFMT,line)) for line in eacsv]
 #print struct.calcsize(EAFMT)
@@ -20,6 +24,8 @@ ea = [map(str.strip,struct.unpack(EAFMT,line)) for line in eacsv]
 dk = list(csv.reader(dkcsv,dialect=csv.excel))
 dkdict = {}
 for item in dk:
+    item.append("") #NOTES
+    item.append("") #ea Key
     dkdict[item[0].strip().upper()]=item
 #print dkdict.keys()
 
@@ -31,7 +37,6 @@ inEAnotDK = []
 for line in ea:
     #Generate key and get dk part number
     device = line[2].strip()
-    line[1] = line[1].upper()
     try:
         value,dknum = map(str.strip,line[1].split(':'))
         key = ":".join([device,value])
@@ -40,22 +45,23 @@ for line in ea:
         notEA.append(line)
         value = ""
         dknum = line[1].strip()
-        key = device
+        key = device + ":"
     dknum = dknum.split('(')[0].strip().upper() #Take the first number if parenthetical values
-    
+    oldNumbers = "/".join(dknum.split('(')[1:]) +" "
     #Prefix to the matching line in the dk csv file
+    keyLower = key
     key = key.upper()
     #print "Key \"%s\""%(key)
     try:
         item = dkdict.pop(dknum)
-        if len(item) <13:
+        if len(item) < COMPLETELINESIZE:
             item.insert(0,key)
+            item[EAKEY] = keyLower
         else:
             dupEA.append(item)
+        item[NOTES] = item[NOTES] + oldNumbers
         dkdict[dknum]=item
-
     except KeyError as ke:
-        #print "Not found in dk \"%s\""%(dknum)
         notDK.append((dknum,key))
 
 print "No value for part in EA. n=%d"%(len(notEA))
@@ -82,15 +88,18 @@ print "Extra parts in DK BOM not found in EA. n=%d"%(len(inEAnotDK))
 for i in inEAnotDK:
     print i[0:3]
 
-#TODO fix LEDCHIPLED_1206:GREEN showing up in eanotdk
+#GENERATE DATABASE FOR EAGLE
+outfile.write("Key,PartNumber,Distributor,Manufacturer,Notes\n")
+for key,item in dkdict.iteritems():
+    if len(item) == COMPLETELINESIZE:
+        line = EAGLEDELIMETER.join([item[EAKEY],key,"Digikey",item[2],item[NOTES] +"\n"])
+        outfile.write(line)
+
+#CLEAN UP EAGLE FILES
+
 #CP-002A-ND needs slightly wider holes. 
     
-##for line in dk[0:3]:
-##    print line
-##print
-##for line in ea[0:3]:
-##    print line
-#print ea.next()
 
-#eacsv.close()
-#dkcsv.close()
+eacsv.close()
+dkcsv.close()
+outfile.close()
